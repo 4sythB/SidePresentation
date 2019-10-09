1749bcbd1178d8672c0726212bf0d6dff6d18b78
Brad Forsyth <b4syth@me.com>
2019-10-08 22:51:42 -0600

    Initial Commit

---
+++ SidePresentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SidePresentation
+//
+//  Created by Brad Forsyth on 10/8/19.
+//  Copyright © 2019 Brad Forsyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
